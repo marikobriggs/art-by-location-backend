71eff5d05d33c8c4fe6543ca137b378d96e56436
Mariko Briggs <mariko.briggs@gmail.com>
2023-01-26 22:53:18 -0800

    remove reference to met api and switch to own; pull api logic into separate function

--- art/src/api/requestArtURL.js
+++ art/src/api/requestArtURL.js
@@ -1,43 +1,47 @@
-const requestArtURL = (filteredLocation) => {
-  let randomObjectID = 0;
-  let url = "sufdajlsdf";
+const requestArtURL = async (filteredLocation) => {
+  // const throwRequestArtURLError = async () => {
+  //   throw new Error("Error requesting art");
+  // };
+  // let url = "";
+  console.log(filteredLocation);
 
   const myRequest = new Request(
-    `https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&geoLocation=${filteredLocation}&q=""`
+    `https://glusk0c208.execute-api.us-west-1.amazonaws.com/items/${filteredLocation}`
   );
 
-  fetch(myRequest)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`HTTP error: ${response.status}`);
-      }
-      return response.json();
-    })
-    .then((response) => {
-      // response is an array of objectIDs, each of which correspond to a piece of art
-      // here we choose a random value from that array and return another fetch call which gets more info on that object
-      let random = Math.floor(Math.random() * response.objectIDs.length);
-      console.log(random, response.objectIDs[random]);
-      randomObjectID = response.objectIDs[random];
+  const response = await fetch(myRequest);
+  const responseBody = await response.json();
+  return await responseBody.URL;
 
-      return fetch(
-        `https://collectionapi.metmuseum.org/public/collection/v1/objects/${randomObjectID}`
-      );
-    }) // TODO: add error handling for second fetch
-    .then((response) => {
-      return response.json();
-    })
-    
-    .then((data) => {
-      console.log(data.primaryImageSmall);
-      // setArtURL(data.primaryImageSmall);
-      console.log("before set" + url) 
-      url = data.primaryImageSmall;
-      console.log("after set" + url);
-      return url;
-    });
-  // console.log("ASKJDFKAJSDFKLJASDFJKSFJ " + url);
-  // return url;
+  // try {
+  //   const response = await fetch(myRequest);
+  //   const responseBody = await response.json();
+  // } catch (err) {
+  //   console.error(err);
+  // } finally {
+  //   console.log("Errors follow?")
+  //   return await responseBody.URL
+  // }
+
+  // return await responseBody.URL;
+
+  // return fetch(myRequest)
+  //   .then((response) => {
+  //     if (!response.ok) {
+  //       throw new Error(`HTTP error: ${response.status}`);
+  //     }
+  //     // console.log("RESPONSE.JSON ====== " + response.json());
+  //     // url = response;
+  //     return response.json();
+  //   })
+  //   .then((response) => {
+  //     // url = response.URL;
+  //     // console.log(url)
+  //     return response.URL;
+  //   })
+  //   .catch((error) => {
+  //     console.error("Fetch error: ", error);
+  //   });
 };
 
 export default requestArtURL;
