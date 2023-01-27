f2d64e5c28651dbc82d7a91d441dabe923c06ccf
Mariko Briggs <mariko.briggs@gmail.com>
2023-01-27 10:18:31 -0800

    remove commented code, update api logic request logic

--- art/src/api/requestArtURL.js
+++ art/src/api/requestArtURL.js
@@ -1,47 +1,26 @@
 const requestArtURL = async (filteredLocation) => {
-  // const throwRequestArtURLError = async () => {
-  //   throw new Error("Error requesting art");
-  // };
-  // let url = "";
   console.log(filteredLocation);
 
   const myRequest = new Request(
     `https://glusk0c208.execute-api.us-west-1.amazonaws.com/items/${filteredLocation}`
   );
 
-  const response = await fetch(myRequest);
-  const responseBody = await response.json();
-  return await responseBody.URL;
+  try {
+    const response = await fetch(myRequest);
+    const responseBody = await response.json();
 
-  // try {
-  //   const response = await fetch(myRequest);
-  //   const responseBody = await response.json();
-  // } catch (err) {
-  //   console.error(err);
-  // } finally {
-  //   console.log("Errors follow?")
-  //   return await responseBody.URL
-  // }
-
-  // return await responseBody.URL;
-
-  // return fetch(myRequest)
-  //   .then((response) => {
-  //     if (!response.ok) {
-  //       throw new Error(`HTTP error: ${response.status}`);
-  //     }
-  //     // console.log("RESPONSE.JSON ====== " + response.json());
-  //     // url = response;
-  //     return response.json();
-  //   })
-  //   .then((response) => {
-  //     // url = response.URL;
-  //     // console.log(url)
-  //     return response.URL;
-  //   })
-  //   .catch((error) => {
-  //     console.error("Fetch error: ", error);
-  //   });
+    if (response.ok) {
+      return await responseBody.URL;
+    } else {
+      throw new Error("Bad network response");
+    }
+  } catch (error) {
+    return error;
+  }
 };
 
 export default requestArtURL;
+
+// const response = await fetch(myRequest);
+// const responseBody = await response.json();
+// return await responseBody.URL;
--- art/src/components/Menu.js
+++ art/src/components/Menu.js
@@ -3,80 +3,47 @@ import ImageDisplay from "./ImageDisplay";
 import LocationFilter from "./LocationFilter";
 import requestArtURL from "../api/requestArtURL";
 
-// TODO: decompose Button
 const Menu = (props) => {
   const [filteredLocation, setFilteredLocation] = useState(props.options[0]);
-  const [artURL, setArtURL] = useState(""); 
+  const [artURL, setArtURL] = useState("");
 
   // TODO: don't use a function for props
   const filterChangeHandler = (selectedLocation) => {
     setFilteredLocation(selectedLocation);
   };
 
-  const buttonClickHandler = (event) => {
+  const buttonClickHandler = async (event) => {
+    console.log("start" + "-".repeat(40));
     (async () => {
-      let url = await requestArtURL(filteredLocation)
-      // setArtURL(await requestArtURL(filteredLocation))
-      setArtURL(await url) 
-    })()
-    console.log("art url set by state = " + artURL)
-    console.log("-".repeat(40));
-    
+      let url = await requestArtURL(filteredLocation);
+      console.log("url that we set state to, inside async = " + url);
 
-  }
-    // setArtURL()
+      // handling error return from api call function.
+      if (url instanceof Error) console.log("Error occured in requesting URL.");
 
-    // let randomObjectID = 0;
-    // console.log(filteredLocation);
-    // console.log("artURL = " + artURL);
-
-    // const myRequest = new Request(
-    //   `https://glusk0c208.execute-api.us-west-1.amazonaws.com/items/${filteredLocation}`
-    // );
-    // fetch(myRequest)
-    //   .then((response) => {
-    //     if (!response.ok) {
-    //       throw new Error(`HTTP error: ${response.status}`);
-    //     }
-    //     return response.json();
-    //   })
-    //   .then((response) => {
-    //     // response is an array of objectIDs, each of which correspond to a piece of art
-    //     // here we choose a random value from that array and return another fetch call which gets more info on that object
-    //     let random = Math.floor(Math.random() * response.objectIDs.length);
-    //     console.log(random, response.objectIDs[random]);
-    //     randomObjectID = response.objectIDs[random];
-
-    //     return fetch(
-    //       `https://collectionapi.metmuseum.org/public/collection/v1/objects/${randomObjectID}`
-    //     );
-    //   })
-    //   .then((response) => {
-    //     return response.json();
-    //   })
-    //   .then((data) => {
-    //     setArtURL(data.primaryImageSmall);
-    //   });
-    // };
-
-    return (
-      <div className="">
-        <LocationFilter
-          options={props.options}
-          selected={filteredLocation}
-          onChangeFilter={filterChangeHandler}
-        />
-        <button
-          className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
-          onClick={() => {
-            buttonClickHandler();
-          }}
-        >
-          Display random art from {filteredLocation}
-        </button>
-        <ImageDisplay className="p-2" url={artURL} />
-      </div>
-    );
+      setArtURL(await url);
+    })();
+    console.log("art url set by state, outside of async = " + artURL);
   };
+
+  return (
+    <div className="">
+      <LocationFilter
+        options={props.options}
+        selected={filteredLocation}
+        onChangeFilter={filterChangeHandler}
+      />
+      <button
+        className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
+        onClick={() => {
+          buttonClickHandler();
+        }}
+      >
+        Display random art from {filteredLocation}
+      </button>
+      <ImageDisplay className="p-2" url={artURL} />
+    </div>
+  );
+};
 
 export default Menu;
