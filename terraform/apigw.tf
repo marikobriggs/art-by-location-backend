5aac35ded72666496cf200e0f54bd450486158d7
Mariko Briggs <mariko.briggs@gmail.com>
2023-02-12 23:45:37 -0800

    update vars, fix incorrectly set values

--- terraform/apigw.tf
+++ terraform/apigw.tf
@@ -25,18 +25,17 @@ resource "aws_apigatewayv2_api" "art_api" {
 
 # aws_apigatewayv2_stage.art_api_default_stage:
 resource "aws_apigatewayv2_stage" "art_api_default_stage" {
-  api_id          = aws_apigatewayv2_api.art_api.id
-  auto_deploy     = true
-  name            = "main"
-  stage_variables = {}
-  tags            = {}
-  tags_all        = {}
+  api_id      = aws_apigatewayv2_api.art_api.id
+  auto_deploy = true
+  name        = "main"
+  tags        = {}
+  tags_all    = {}
 
   default_route_settings {
     data_trace_enabled       = false
     detailed_metrics_enabled = false
-    throttling_burst_limit   = 0
-    throttling_rate_limit    = 0
+    throttling_burst_limit   = 1000
+    throttling_rate_limit    = 1000
   }
 }
 
@@ -47,10 +46,9 @@ resource "aws_apigatewayv2_integration" "art_api_lambda_integration" {
   integration_method = "POST"
   integration_type   = "AWS_PROXY"
   # integration_uri        = "arn:aws:lambda:us-west-1:687391720917:function:art-api"
-  integration_uri        = aws_lambda_function.art_api_lambda.arn
+  integration_uri        = aws_lambda_function.art_api_lambda.invoke_arn
+  # integration_uri        = aws_lambda_function.art_api_lambda.qualified_invoke_arn
   payload_format_version = "2.0"
-  request_parameters     = {}
-  request_templates      = {}
   timeout_milliseconds   = 30000
 }
 
@@ -62,6 +60,7 @@ resource "aws_apigatewayv2_route" "art_api_get" {
   authorization_type   = "NONE"
   request_models       = {}
   route_key            = "GET /items/{CountryName}"
-  target               = "integrations/${aws_apigatewayv2_api.art_api.id}"
+  # route_key = "/items/{CountryName}"
+  target = "integrations/${aws_apigatewayv2_integration.art_api_lambda_integration.id}"
 }
 
